965ccd779f95a758e1867d0d1859d7054cb45321
Neil <zooming.in@hotmail.com>
2021-09-02 00:08:40 +1000

    updates

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ActiveRecord::Base
+    # it 'has a summary' do
+    #     post = Post.create!(title: "My title", description: "The post description")
+    #     expect(post.post_summary).to eq("My title - The post description")
+    #   end
+
+    # def post_summary
+    #     self.title + " - " + self.description
+    # end
+      
+
+
+
+end
